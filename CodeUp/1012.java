6b51fc05cbe4dc0c470a83d55a094b8cfc5b9264
HelloJinny <65798207+HelloJinny@users.noreply.github.com>
2021-03-02 04:24:22 +0900

    Add files via upload

---
+++ CodeUp/1012.java
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		float f;
+		
+		Scanner sc = new Scanner(System.in);
+		f = sc.nextFloat();
+		
+		System.out.printf("%f", f);
+
+	}
+
+}
---
+++ CodeUp/1024.java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+
+		String[] array_words = new String[word.length()];		
+		array_words = word.split("");
+		
+		for (int i = 0; i < word.length(); i++)
+			System.out.printf("'%s'\n",array_words[i]);
+		
+	}
+
+}
